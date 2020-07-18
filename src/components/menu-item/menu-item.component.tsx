82fa383e172b89aaa435112e254cddf0c4bf11de
Nozomu Tsuruta <61049074+NozomuTsuruta@users.noreply.github.com>
2020-07-19 00:51:23 +0900

    'homepageの完成'

---
+++ src/components/menu-item/menu-item.component.tsx
@@ -0,0 +1,28 @@
+import React, { FC } from 'react';
+
+import './menu-item.styles.scss';
+
+type Props = {
+  title: string;
+  imageUrl: string;
+  size?: string;
+};
+
+const MenuItem: FC<Props> = ({ title, imageUrl, size }) => {
+  return (
+    <div className={`${size} menu-item`}>
+      <div
+        className='background-image'
+        style={{
+          backgroundImage: `url(${imageUrl})`,
+        }}
+      />
+      <div className='content'>
+        <h1 className='title'>{title.toUpperCase()}</h1>
+        <span className='subtitle'>SHOP NOW</span>
+      </div>
+    </div>
+  );
+};
+
+export default MenuItem;
