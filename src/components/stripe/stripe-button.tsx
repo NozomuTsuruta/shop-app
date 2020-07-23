3c63d9b9b615eb155620e89e5762331835e92d14
Nozomu Tsuruta <61049074+NozomuTsuruta@users.noreply.github.com>
2020-07-24 07:07:21 +0900

    'stripeのts化'

--- src/components/stripe/stripe-button.tsx
+++ src/components/stripe/stripe-button.tsx
@@ -1,11 +1,15 @@
-import React from 'react'
+import React, { FC } from 'react'
 import StripeCheckout from 'react-stripe-checkout'
 
-const StripeCheckoutButton = ({price}) => {
+type Props = {
+  price: number
+}
+
+const StripeCheckoutButton:FC<Props> = ({price}) => {
   const priceForStripe = price*100
   const publishableKey = 'pk_test_51H8BYeKgU3RvaoWrTLE7dYjUnSWQWmJR5Of0XmTUOpvrTTW5YsGesgQCUnxbs3pHLRn2jOxJi4LvA7o9W6HA7ldj00tpGWPQAW'
 
-  const onToken=token=>{
+  const onToken=(token: any)=>{
     console.log(token)
     alert('Payment Successful')
   }
