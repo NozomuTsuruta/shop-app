6943109a566908719e02bc93ee2412826fdf900f
Nozomu Tsuruta <61049074+NozomuTsuruta@users.noreply.github.com>
2020-07-24 07:25:14 +0900

    'stripeのts化'

--- src/components/stripe/stripe-button.tsx
+++ src/components/stripe/stripe-button.tsx
@@ -1,18 +1,19 @@
-import React, { FC } from 'react'
-import StripeCheckout from 'react-stripe-checkout'
+import React, { FC } from 'react';
+import StripeCheckout from 'react-stripe-checkout';
 
 type Props = {
-  price: number
-}
+  price: number;
+};
 
-const StripeCheckoutButton:FC<Props> = ({price}) => {
-  const priceForStripe = price*100
-  const publishableKey = 'pk_test_51H8BYeKgU3RvaoWrTLE7dYjUnSWQWmJR5Of0XmTUOpvrTTW5YsGesgQCUnxbs3pHLRn2jOxJi4LvA7o9W6HA7ldj00tpGWPQAW'
+const StripeCheckoutButton: FC<Props> = ({ price }) => {
+  const priceForStripe = price * 100;
+  const publishableKey =
+    'pk_test_51H8BYeKgU3RvaoWrTLE7dYjUnSWQWmJR5Of0XmTUOpvrTTW5YsGesgQCUnxbs3pHLRn2jOxJi4LvA7o9W6HA7ldj00tpGWPQAW';
 
-  const onToken=(token: any)=>{
-    console.log(token)
-    alert('Payment Successful')
-  }
+  const onToken = (token: any) => {
+    console.log(token);
+    alert('Payment Successful');
+  };
 
   return (
     <StripeCheckout
@@ -27,8 +28,7 @@ const StripeCheckoutButton:FC<Props> = ({price}) => {
       token={onToken}
       stripeKey={publishableKey}
     />
-  )
-}
+  );
+};
 
-export default StripeCheckoutButton
-
+export default StripeCheckoutButton;
